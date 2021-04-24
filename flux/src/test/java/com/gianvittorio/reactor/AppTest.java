4b6bfbe7b77b177713aef4053748b00247290c56
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-04-24 14:19:29 -0300

    add project

---
+++ flux/src/test/java/com/gianvittorio/reactor/AppTest.java
@@ -0,0 +1,21 @@
+package com.gianvittorio.reactor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat(true)
+                .isTrue();
+    }
+}
