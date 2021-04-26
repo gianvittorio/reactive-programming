ebf285d18b7bd52871efd3a5483db3e987e5d877
gianvittorio <gianvittorio.castellano@fonyou.com>
2021-04-25 21:32:03 -0300

    done with parallel flux and parallelization using flatMap

--- flux/src/main/java/com/gianvittorio/reactor/service/FluxAndMonoSchedulerService.java
+++ flux/src/main/java/com/gianvittorio/reactor/service/FluxAndMonoSchedulerService.java
@@ -2,6 +2,8 @@ package com.gianvittorio.reactor.service;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.ParallelFlux;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
@@ -18,14 +20,12 @@ public class FluxAndMonoSchedulerService {
     public Flux<String> explorePublishOn() {
         Flux<String> namesFlux = Flux.fromIterable(namesList)
                 .publishOn(Schedulers.parallel())
-                .map(this::uppercase)
-                .doOnNext(name -> log.info("name is: {}", name));
+                .map(this::uppercase);
 
         Flux<String> namesFlux1 = Flux.fromIterable(nameslist1)
                 .publishOn(Schedulers.parallel())
-                .map(this::uppercase)
-                .doOnNext(name -> log.info("name is: {}", name));
-
+                .map(this::uppercase);
+        
         return namesFlux.mergeWith(namesFlux1);
     }
 
@@ -35,8 +35,53 @@ public class FluxAndMonoSchedulerService {
                 .doOnNext(name -> log.info("name is: {}", name));
 
         Flux<String> namesFlux1 = getMap()
-                .subscribeOn(Schedulers.boundedElastic())
-                .doOnNext(name -> log.info("name is: {}", name));
+                .subscribeOn(Schedulers.boundedElastic());
+
+        return namesFlux.mergeWith(namesFlux1);
+    }
+
+    public ParallelFlux<String> exploreParallel() {
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        log.info("Available processors: {}", availableProcessors);
+
+        return Flux.fromIterable(namesList)
+                .parallel()
+                .runOn(Schedulers.parallel())
+                .map(this::uppercase);
+    }
+
+    public Flux<String> exploreParallelUsingFlatMap() {
+        return Flux.fromIterable(namesList)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::uppercase)
+                            .subscribeOn(Schedulers.parallel());
+                });
+    }
+
+    public Flux<String> exploreParallelUsingFlatMapSequential() {
+        return Flux.fromIterable(namesList)
+                .flatMapSequential(name -> {
+                    return Mono.just(name)
+                            .map(this::uppercase)
+                            .subscribeOn(Schedulers.parallel());
+                });
+    }
+
+    public Flux<String> exploreParallelUsingFlatMap1() {
+        Flux<String> namesFlux = Flux.fromIterable(namesList)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::uppercase)
+                            .subscribeOn(Schedulers.parallel());
+                });
+
+        Flux<String> namesFlux1 = Flux.fromIterable(nameslist1)
+                .flatMap(name -> {
+                    return Mono.just(name)
+                            .map(this::uppercase)
+                            .subscribeOn(Schedulers.parallel());
+                });
 
         return namesFlux.mergeWith(namesFlux1);
     }
--- flux/src/test/java/com/gianvittorio/reactor/service/FluxAndMonoSchedulerServiceTest.java
+++ flux/src/test/java/com/gianvittorio/reactor/service/FluxAndMonoSchedulerServiceTest.java
@@ -3,6 +3,7 @@ package com.gianvittorio.reactor.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.ParallelFlux;
 import reactor.test.StepVerifier;
 
 public class FluxAndMonoSchedulerServiceTest {
@@ -40,5 +41,61 @@ public class FluxAndMonoSchedulerServiceTest {
                 .expectSubscription()
                 .expectNextCount(6)
                 .verifyComplete();
+    }
+
+    @Test
+    public void exploreParallelTest() {
+        // Given
+
+        // When
+        ParallelFlux<String> flux = fluxAndMonoSchedulerService.exploreParallel();
+
+        // Then
+        StepVerifier.create(flux.log())
+                .expectSubscription()
+                .expectNextCount(3)
+                .expectComplete();
+    }
+
+    @Test
+    public void exploreParallelUsingFlatMapTest() {
+        // Given
+
+        // When
+        Flux<String> flux = fluxAndMonoSchedulerService.exploreParallelUsingFlatMap();
+
+        // Then
+        StepVerifier.create(flux.log())
+                .expectSubscription()
+                .expectNextCount(3)
+                .expectComplete();
+    }
+
+    @Test
+    public void exploreParallelUsingFlatMapSequentialTest() {
+        // Given
+
+        // When
+        Flux<String> flux = fluxAndMonoSchedulerService.exploreParallelUsingFlatMapSequential();
+
+        // Then
+        StepVerifier.create(flux.log())
+                .expectSubscription()
+                .expectNext("ALEX", "BEN", "CHLOE")
+                .expectComplete();
+    }
+
+    @Test
+    public void exploreParallelUsingFlatMap1Test() {
+        // Given
+
+        // When
+        Flux<String> flux = fluxAndMonoSchedulerService.exploreParallelUsingFlatMap1();
+
+        // Then
+        StepVerifier.create(flux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .expectComplete();
     }
 }
